7227b82d7573e1d447e77865d0170a818ab465cb
pavel jašek <paveljasek@gmail.com>
2014-08-31 00:25:05 +0200

    best.R working OK

--- best.R
+++ best.R
@@ -3,7 +3,26 @@
 
 best <- function(state, outcome) {
   ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
   ## Check that state and outcome are valid
+  states <- unique(data$State) #check against the actual data
+  if ((state %in% states) == FALSE) {
+    stop("invalid state")
+  }
+  if (!(outcome %in% c("pneumonia", "heart attack", "heart failure"))) {
+    stop("invalid outcome")
+  }
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+  if (outcome == "pneumonia") {
+    data.state <- data[data$State == state,c(2,23)]
+  } else if (outcome == "heart attack") {
+    data.state <- data[data$State == state,c(2,11)]
+  } else if (outcome == "heart failure") {
+    data.state <- data[data$State == state,c(2,17)]
+  }
+  data.state[, 2] <- suppressWarnings(as.numeric(data.state[, 2]))
+  data.state.ordered <- data.state[order(data.state[2], na.last = NA),] #lowest 30-day death rate
+  data.state.ordered[1,1]
 }
--- first-steps.R
+++ first-steps.R
@@ -7,3 +7,31 @@ str(outcome)
 
 outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
+
+
+states <- unique(outcome$State)
+"MD" %in% states
+!("MD" %in% states)
+"BB" %in% states
+if (states["MD"]) {
+  message("jo")
+}
+
+names(outcome)
+
+
+# try out best.R ----------------------------------------------------------
+
+source("best.R")
+best("TX", "heart attack")
+best("TX", "heart failure")
+best("MD", "heart attack")
+best("MD", "pneumonia")
+best("BB", "heart attack")
+best("NY", "hert attack")
+
+
+# submitting --------------------------------------------------------------
+
+source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript3.R")
+submit()
